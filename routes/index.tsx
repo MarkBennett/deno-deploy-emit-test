b87077befd4e8560970d002bb6fd41b850c8201b
Mark Bennett <mark@burmis.ca>
2022-08-26 06:48:16 -0600

    Initial commit

---
+++ routes/index.tsx
@@ -0,0 +1,16 @@
+/** @jsx h */
+import { h } from "preact";
+import { tw } from "@twind";
+
+export default function Home() {
+  return (
+    <div class={tw`p-4 mx-auto max-w-screen-md`}>
+      <img
+        src="/logo.svg"
+        height="100px"
+        alt="the fresh logo: a sliced lemon dripping with juice"
+      />
+      <p class={tw`my-6`}>Welcome to `fresh`.</p>
+    </div>
+  );
+}
